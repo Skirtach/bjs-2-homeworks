08435cc35142ce70ba23c502a5f51ace172b2def
Irina Skirtach <ir_si@e1.ru>
2022-05-20 19:25:08 +0500

    Second commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  
 
-  // Ваш код
+  let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+
+  return result
+
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  
 
-  // Ваш код
+  let resultArr = arr.filter((num)=> num > 0).filter((num)=> num % 3 == 0).map((num)=> num*10);
+  
 
   return resultArr; // array
 }
