ba8c380c77dcbc6b42ad912b6c6f7d1df849e3ed
Skirtach Irina <ir_si@e1.ru>
2022-06-08 21:41:34 +0500

    commit end

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,15 +2,16 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr;
-  let d = Math.pow(b, 2)-4*a*c;
-  if (d<0) 
-  return arr = [];
-  else if (d===0)
-  return arr = [-b/(2*a)];
-  else (d>0)
-  return arr = [-b + Math.sqrt(d) /(2*a),-b - Math.sqrt(d) /(2*a)];
  
+  let d = Math.pow(b, 2)-4*a*c;
+  if (d===0) {
+    return [-b/(2*a)];
+  }
+  else if (d > 0){
+    return [(-b + Math.sqrt(d)) /(2*a), (-b - Math.sqrt(d))/(2*a)];
+  }
+
+  return  []; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
